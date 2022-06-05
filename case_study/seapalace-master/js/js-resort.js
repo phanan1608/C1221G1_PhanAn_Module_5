5c23e9af5ca4a0b8795fe9be2e9df87893dae011
phanan1608 <96655589+phanan1608@users.noreply.github.com>
2022-06-05 20:27:17 +0700

    case study update

---
+++ case_study/seapalace-master/js/js-resort.js
@@ -0,0 +1,10 @@
+
+document.querySelector('.card-all').onclick = ({
+                                                        target
+                                                    }) => {
+    if (!target.classList.contains('more')) return;
+    document.querySelectorAll('.dropout.active').forEach(
+        (d) => d !== target.parentElement && d.classList.remove('active')
+    );
+    target.parentElement.classList.toggle('active');
+};
